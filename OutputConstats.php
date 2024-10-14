10ad666455925b7427f79c0d4eda4ac5e646c8fc
Keane <keaneclement40@gmail.com>
2024-10-14 21:14:29 +0300

    Second  commit

--- OutputConstats.php
+++ OutputConstats.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Employee Information</title>
+    <title>Keane Clement</title>
 </head>
 
 <body>
